988de3c94a29e0fede99db6cdca26e537afddb3d
Cindy Chau <117945543+cindychau1@users.noreply.github.com>
2023-04-06 10:05:19 -0400

    added styling

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -4,7 +4,13 @@ import chatmate from '../../../assets/logo-no-background.png';
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <img src={chatmate} alt='chatmate-logo' width='110' height='60' />
+      <img
+        className='logo'
+        src={chatmate}
+        alt='chatmate-logo'
+        width='110'
+        height='60'
+      />
     </nav>
   );
 };
